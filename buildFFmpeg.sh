ec18c2000c1750a8086119b1276ff7e913ec28d9
liji <liji@meixing.com>
2018-10-15 21:34:14 +0800

    x86

--- buildFFmpeg.sh
+++ buildFFmpeg.sh
@@ -26,7 +26,7 @@ COMMON_OPTIONS="\
     --disable-avfilter \
     --disable-symver \
     --enable-swresample \
-    --enable-avresample \
+
     --enable-decoder=ape \
     --enable-demuxer=ape \
     --enable-decoder=mp3 \
@@ -36,18 +36,18 @@ COMMON_OPTIONS="\
 cd "${FFMPEG_EXT_PATH}/jni" && \
 
 cd ffmpeg && \
-#./configure \
-#    --libdir=../android-libs/armeabi-v7a \
-#    --incdir=../include \
-#    --arch=arm \
-#    --cpu=armv7-a \
-#    --cross-prefix="${NDK_PATH}/toolchains/arm-linux-androideabi-4.9/prebuilt/${HOST_PLATFORM}/bin/arm-linux-androideabi-" \
-#    --sysroot="${NDK_PATH}/platforms/android-9/arch-arm/" \
-#    --extra-cflags="-march=armv7-a -mfloat-abi=softfp" \
-#    --extra-ldflags="-Wl,--fix-cortex-a8" \
-#    --extra-ldexeflags=-pie \
-#    ${COMMON_OPTIONS} \
-#    && \
+./configure \
+    --libdir=../android-libs/armeabi-v7a \
+    --incdir=../include \
+    --arch=arm \
+    --cpu=armv7-a \
+    --cross-prefix="${NDK_PATH}/toolchains/arm-linux-androideabi-4.9/prebuilt/${HOST_PLATFORM}/bin/arm-linux-androideabi-" \
+    --sysroot="${NDK_PATH}/platforms/android-9/arch-arm/" \
+    --extra-cflags="-march=armv7-a -mfloat-abi=softfp" \
+    --extra-ldflags="-Wl,--fix-cortex-a8" \
+    --extra-ldexeflags=-pie \
+    ${COMMON_OPTIONS} \
+    && \
 #make -j4 && make install && \
 
 make clean && ./configure \
