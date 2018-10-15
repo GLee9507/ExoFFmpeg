d25bec69370c79d49fe9dfff36ed5550b1234a2f
liji <liji@meixing.com>
2018-10-15 18:44:50 +0800

    add ffmpeg x86 so

--- buildFFmpeg.sh
+++ buildFFmpeg.sh
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
 cd "./ExoPlayer"
 EXOPLAYER_ROOT="$(pwd)"
 
 FFMPEG_EXT_PATH="${EXOPLAYER_ROOT}/extensions/ffmpeg/src/main"
 
-NDK_PATH="/home/glee/Android/android-ndk-r15c/"
+#Manjaro
+#NDK_PATH="/home/glee/Android/android-ndk-r15c"
+
+#WSL
+NDK_PATH="/mnt/d/ubuntu/android-ndk"
 
 HOST_PLATFORM="linux-x86_64"
 
@@ -22,12 +27,6 @@ COMMON_OPTIONS="\
     --disable-symver \
     --enable-swresample \
     --enable-avresample \
-    --enable-decoder=vorbis \
-    --enable-demuxer=vorbis \
-    --enable-decoder=opus \
-    --enable-demuxer=opus \
-    --enable-decoder=flac \
-    --enable-demuxer=flac \
     --enable-decoder=ape \
     --enable-demuxer=ape \
     --enable-decoder=mp3 \
@@ -35,21 +34,32 @@ COMMON_OPTIONS="\
     --enable-protocol=file \
     " && \
 cd "${FFMPEG_EXT_PATH}/jni" && \
-(git -C ffmpeg pull || git clone git://source.ffmpeg.org/ffmpeg ffmpeg) && \
+
 cd ffmpeg && \
-./configure \
-    --libdir=../android-libs/armeabi-v7a \
-    --incdir=../include \
-    --arch=arm \
-    --cpu=armv7-a \
-    --cross-prefix="${NDK_PATH}/toolchains/arm-linux-androideabi-4.9/prebuilt/${HOST_PLATFORM}/bin/arm-linux-androideabi-" \
-    --sysroot="${NDK_PATH}/platforms/android-9/arch-arm/" \
-    --extra-cflags="-march=armv7-a -mfloat-abi=softfp" \
-    --extra-ldflags="-Wl,--fix-cortex-a8" \
+#./configure \
+#    --libdir=../android-libs/armeabi-v7a \
+#    --incdir=../include \
+#    --arch=arm \
+#    --cpu=armv7-a \
+#    --cross-prefix="${NDK_PATH}/toolchains/arm-linux-androideabi-4.9/prebuilt/${HOST_PLATFORM}/bin/arm-linux-androideabi-" \
+#    --sysroot="${NDK_PATH}/platforms/android-9/arch-arm/" \
+#    --extra-cflags="-march=armv7-a -mfloat-abi=softfp" \
+#    --extra-ldflags="-Wl,--fix-cortex-a8" \
+#    --extra-ldexeflags=-pie \
+#    ${COMMON_OPTIONS} \
+#    && \
+#make -j4 && make install && \
+
+make clean && ./configure \
+    --libdir=../android-libs/x86 \
+    --arch=x86 \
+    --cpu=i686 \
+    --cross-prefix="${NDK_PATH}/toolchains/x86-4.9/prebuilt/${HOST_PLATFORM}/bin/i686-linux-android-" \
+    --sysroot="${NDK_PATH}/platforms/android-9/arch-x86/" \
     --extra-ldexeflags=-pie \
+    --disable-asm \
     ${COMMON_OPTIONS} \
     && \
-make -j4 && make install && \
-
+make -j4 && make install-libs && \
 
 make clean
